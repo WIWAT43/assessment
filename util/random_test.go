3496fbcdbeae2d7cd2cbb240846670b3fbd6fe1d
wiwat_43 <wiwat.sathornsawat@accenture.com>
2022-12-31 16:26:03 +0700

    Add util test

---
+++ util/random_test.go
@@ -0,0 +1,26 @@
+package util
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func Test_RandomInt(t *testing.T) {
+	randomNumber := RandomInt(10, 100)
+
+	require.NotEmpty(t, randomNumber)
+
+	checker := false
+	if (randomNumber >= 10) && (randomNumber <= 100) {
+		checker = true
+	}
+
+	require.Equal(t, true, checker)
+}
+
+func Test_RandomString(t *testing.T) {
+	randomString := RandomString(100)
+
+	require.NotEmpty(t, randomString)
+	require.Equal(t, 100, len(randomString))
+}
